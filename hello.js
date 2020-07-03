cdde0b84a9741c73aef45636e781289e4d7ac034
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-03 16:41:52 +0000

    make use of function to evaluate even number

--- hello.js
+++ hello.js
@@ -10,3 +10,35 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting2 = returnSayHello('John');
+ 
+
+ 
+
+const returnSayHello2  = function (name) {
+  return "Hello, " + name;
+}
+const greeting3 = returnSayHello2('John');
+console.log(greeting3);
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+console.log(isEven(10));
+console.log(isEven(11));
