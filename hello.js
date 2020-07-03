b746dbb76ff1310f39a967c2b73b10a85558acb8
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-03 16:48:02 +0000

    remove even function and create a new file

--- hello.js
+++ hello.js
@@ -29,16 +29,3 @@ const returnSayHello2  = function (name) {
 }
 const greeting3 = returnSayHello2('John');
 console.log(greeting3);
-
-const isEven  = function (num) {
-  return num % 2 === 0;
-}
-
-const tenIsEven = isEven(10);
-const elevenIsEven = isEven(11);
-
-console.log(tenIsEven);
-console.log(elevenIsEven);
-
-console.log(isEven(10));
-console.log(isEven(11));
